4f66b0122ebd992d2e2cd216a26bb0a02e6c9fcb
susumu <susumu@iorc.net>
2022-08-15 22:27:33 +0900

    conflict対策2

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
-          Edit <code>src/App.js</code> and save to reload.
-=======
+
           <code>src/App.js</code> and save to reload.
->>>>>>> true-win-Hot-reload
         </p>
         <a
           className="App-link"
