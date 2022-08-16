19936343645a1a892b8e54b70d2c985b4141fab8
susumu <susumu@iorc.net>
2022-08-16 14:58:41 +0900

    React18.2.0 -> 17.0.2 & MaterialUi install

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formContainer">
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input type="text" placeholder="メールアドレス" name="mailAddress" />
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input type="text" placeholder="パスワード" name="password" />
+          </div>
+          <button className= "submitButton">ログイン</button>
+        </div>
+      </form>
     </div>
   );
 }
