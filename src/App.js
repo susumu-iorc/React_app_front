ae48a173886275ac6deda3371ea64917a79f1d05
susumu <s.osawa@famitra.jp>
2022-09-02 15:21:55 +0900

    ログインログアウトの処理とりあえず完成

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Cookies from 'universal-cookie';
 import * as CONSTANTS from "./constants.js";
 import makeHeaderToken from "./utility/makeHeaderToken";
 
 const App = () => {
+  // クッキーを利用する
   const cookies = new Cookies();
-  const [apiUserTokens, setApiUserTokens]   = useState({token: cookies.get("access-token"), client: cookies.get("client"), uid: cookies.get("uid")})
 
-  const [loggedInStatus, setLoggedInStatus] = useState(null)
-  const [user, setUser] = useState({})
+  // クッキーに過去のログイン情報があれば取得しておく
+  const [apiUserTokens, setApiUserTokens]   = useState({token: cookies.get("access-token"), client: cookies.get("client"), uid: cookies.get("uid")});
+ 
+  // ログイン状況の宣言
+  const [loggedInStatus, setLoggedInStatus] = useState(null);
 
+  // apiのトークンを管理する
   const updateApiUserTokens = (__token, __client , __uid) => {
-    cookies.set("access-token", __token,)
-    cookies.set("client", __client)
-    cookies.set("uid", __uid)
-    setApiUserTokens({token: __token, client: __client, uid: __uid})
-  }
+    cookies.set("access-token", __token,);
+    cookies.set("client", __client);
+    cookies.set("uid", __uid);
+    setApiUserTokens({token: __token, client: __client, uid: __uid});
+  };
   
+  // ログイン成功時の処理
   const handleLogin = (data) => {
-    updateApiUserTokens(data["access-token"], data["client"], data["uid"])
-    checkLogin()
-  }
+    updateApiUserTokens(data["access-token"], data["client"], data["uid"]);
+    checkLogin();
+  };
+
+  // ログアウト時の処理
   const handleLogout = (data) => {
-    updateApiUserTokens("", "", "")
-    checkLogin()
+    updateApiUserTokens("", "", "");
+    checkLogin();
   }
 
-// 追加
-useEffect(() => {
-  console.log(makeHeaderToken(apiUserTokens))
-  checkLogin()
-})
+  //　ログインチェック
+  const checkLogin = () => {
+    axios.get(CONSTANTS.API_USERBASE_GET_FULL_PATH, { withCredentials: true,headers: makeHeaderToken(apiUserTokens)})
+      .then(response => {
+        setLoggedInStatus(true)
+      }
+      ).catch(error => {
+        setLoggedInStatus(false)
+    });
+  };
 
-// 追加
-const checkLogin = () => {
-  axios.get(CONSTANTS.API_USERBASE_GET_FULL_PATH, { withCredentials: true,headers: makeHeaderToken(apiUserTokens)})
-    .then(response => {
-    setLoggedInStatus(true)
-  }).catch(error => {
-    setLoggedInStatus(false)
-  })
-  console.log("ログイン処理完了 =>" , loggedInStatus)
-}
+  // 画面開いたときに回す処理
+  useEffect(() => {
+   checkLogin(); //ログインしているかの確認
+  });
 
 
   return (
@@ -57,11 +62,11 @@ const checkLogin = () => {
       <Routes>
         <Route path={'/'} element={<Home loggedInStatus={loggedInStatus} handleLogin={handleLogin} handleLogout={handleLogout} apiUserTokens={apiUserTokens} />} />
         <Route path={'/register/'} element={<Register />} />
-        <Route path={'/login/'} element={<Login loggedInStatus={loggedInStatus} handleLogin={handleLogin}/>} />
+        <Route path={'/login/'} element={<Login loggedInStatus={loggedInStatus} handleLogin={handleLogin} apiUserTokens={apiUserTokens}/>} />
         <Route path={"/dashboard"} element={<Dashboard loggedInStatus={loggedInStatus} handleLogin={handleLogin}/>} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 };
 
 export default App;
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -9,8 +9,7 @@ export default function Logout(props) {
 
   const handleLogoutBtn = (event) => {
     // delete
-    console.log("ログアウト処理開始")
-    console.log("トークン→",makeHeaderToken)
+
     axios.delete( CONSTANTS.API_SIGNOUT_FULL_PATH,{ withCredentials: true,headers: makeHeaderToken(props.apiUserTokens)})
         .then(response => {
         // 成功した場合
@@ -27,7 +26,7 @@ export default function Logout(props) {
       })
     event.preventDefault()
 
-    console.log("ログアウト処理終了")
+
   }
 
     return (
