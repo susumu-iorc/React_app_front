4b1ba73ea3b7f68abe45e74e6696c5d080da7c24
susumu <s.osawa@famitra.jp>
2022-09-07 18:36:07 +0900

    memoの内容を表示した

--- src/components/Shoppage.js
+++ src/components/Shoppage.js
@@ -3,23 +3,41 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import * as CONSTANTS from "../constants.js";
 import makeHeaderToken from "../utility/makeHeaderToken";
+
+// memo取得
+
+
 export default function Shoppage(props){
+    const [ memo, setMemo] = useState()
     const { placeId } = useParams();
-    const getMemo = (pid) => {
-        axios.get(CONSTANTS.API_MEMO_GET_FULL_PATH + pid, {withCredentials: true,headers: makeHeaderToken(props.apiUserTokens)})
+
+    const getMemo = (pid, apiUserTokens) => {
+
+        axios.get(CONSTANTS.API_MEMO_GET_FULL_PATH + pid, {withCredentials: true,headers: makeHeaderToken(apiUserTokens)})
           .then(response => {
-            console.log(response.data)
+            if(response.data["success"]){
+                setMemo(response.data["data"])
+            }else{
+                setMemo(null)
+            }
           }
           ).catch(error => {
     
         });
       };
 
+    getMemo(placeId, props.apiUserTokens)
+
+
  
-        getMemo(placeId)
+        
   return (
     <div>
-      placeid: {placeId}
+      店名: {memo["shop-name"]}
+      住所: {memo["shop-address"]}
+      count: {memo["count"]}
+      favo: {memo["favorite"]}
+      memo: {memo["memo"]}
     </div>
   )
 }
