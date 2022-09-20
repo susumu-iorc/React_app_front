03dc645da46fa9a397fc9d297285ea3410b4d7de
susumu <s.osawa@famitra.jp>
2022-09-20 17:01:51 +0900

    未ログイン時のBaseの処理

--- src/pages/Base.js
+++ src/pages/Base.js
@@ -1,51 +1,45 @@
-import { Link, useNavigate, Navigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import axios from 'axios';
 import * as CONSTANTS from "../constants.js";
-import React, { useState } from 'react';
+import React, { useEffect  } from 'react';
 import makeHeaderToken from "../utility/makeHeaderToken";
 import {BaseFormSet} from "../components/BaseFormSet"
 
 const Base = (props) => {
-	console.log("props -> ",props.loggedInStatus);
-	const navigate = useNavigate();  
-//useEffect = (() =>{
-	//if(!props.loggedInStatus){
-    //navigate("/")
-	//};
-//});
+	const navigate = useNavigate()
 
+	useEffect(() => {
+		if(!props.loggedInStatus){
+    	window.location.href = "/"
+		};
+	},[]);
 
+	const handleSubmit = (event) => {
+		axios.post( CONSTANTS.API_USERBASE_UPDATE_FULL_PATH,
+			{
+				"user-post-code"     : props.userBase["userPostCode"],
+				"user-pref"     : props.userBase["userPref"],
+				"user-city"     : props.userBase["userCity"],
+				"user-area"     : props.userBase["userArea"]
+					
+			},
+			{ withCredentials: true,headers: makeHeaderToken(props.apiUserTokens)}
+			).then(response => {
+				// 成功した場合
+				console.log("registration res", response)
+				if (response.data["access-token"] !== null) {
 
-const handleSubmit = (event) => {
-	axios.post( CONSTANTS.API_USERBASE_UPDATE_FULL_PATH,
-		{
-			"user-post-code"     : props.userBase["userPostCode"],
-			"user-pref"     : props.userBase["userPref"],
-			"user-city"     : props.userBase["userCity"],
-			"user-area"     : props.userBase["userArea"]
-				
-		},
-		{ withCredentials: true,headers: makeHeaderToken(props.apiUserTokens)}
-		).then(response => {
-			// 成功した場合
-			console.log("registration res", response)
-			if (response.data["access-token"] !== null) {
+					console.log("へっだー: ", response.headers)
+					navigate("/")
+				}
+			}).catch(error => {
+				// 失敗した場合
+				console.log("registration error", error)
+			})
+		event.preventDefault()
+	}
 
-				console.log("へっだー: ", response.headers)
-				navigate("/dashboard")
-			}
-		}).catch(error => {
-			// 失敗した場合
-			console.log("registration error", error)
-		})
-	event.preventDefault()
-}
   return (
-<>        <div>
-           <p>住所登録</p>
-
-           {/* onSubmit、onChangeイベントを追加 */}
-
            <BaseFormSet 
                   userPostCodeValue={props.userBase["userPostCode"]}
                   userPostCodeOnChange={event => props.setUserBase((prevState) => ({ ...prevState, userPostCode: event.target.value}))}
@@ -58,13 +52,6 @@ const handleSubmit = (event) => {
                   onClickTo={handleSubmit}
                  />
           
-        </div>
-	<div>
-            <h1>Home</h1>
-            <h2>ログイン状態: {props.loggedInStatus ? "ログイン" : "ログアウト"}</h2>
-            <Link to="/">Homeへ</Link>
-	</div>
-</>
   );
 };
 
